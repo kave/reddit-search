6480d07d7a0ab3599b2b0c7f123ef4d276ac7104
etapau <etapau@gmail.com>
2015-07-23 01:09:27 -0400

    heroku test

--- app/main.js
+++ app/main.js
@@ -2,16 +2,30 @@
 var React = require('react/addons');
 var SearchBox = require('./components/SearchBox');
 var request = require('request');
+var $ = require('jQuery');
 
-request('http://reddit-search.herokuapp.com/reddit/', function (error, response, data) {
-    if(data != "" && data != null)
-        data = JSON.parse(data);
-    else
-        data = [];
-    React.render(
-        <SearchBox data={data}/>, document.getElementById('search')
-    );
-});
+
+$.get('/reddit/',
+    function (data) {
+        if(data == "" || data == null)
+            data = [];
+
+        //console.log(data);
+
+        React.render(
+            <SearchBox data={data}/>, document.getElementById('search')
+        );
+    });
+
+//request('http://reddit-search.herokuapp.com/reddit/', function (error, response, data) {
+//    if(data != "" && data != null)
+//        data = JSON.parse(data);
+//    else
+//        data = [];
+//    React.render(
+//        <SearchBox data={data}/>, document.getElementById('search')
+//    );
+//});
 
 
 
