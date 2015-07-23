3a06a94b8d92438082f779dfa992bfe1a984bb81
etapau <etapau@gmail.com>
2015-07-23 01:11:14 -0400

    unique index for mongoDB

--- app/models/RedditSub.js
+++ app/models/RedditSub.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 
 var redditSubSchema = new mongoose.Schema({
     subreddit: String,
-    name: String,
+    name: { type : String , unique : true, required : true},
     link: String,
     text: String,
     createdAt: { type: Date, expires: '1h', default: Date.now }
