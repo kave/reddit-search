7b1cefdf755c971ca79f9b6eea40f9e6a93fdba0
etapau <etapau@gmail.com>
2015-07-23 01:15:54 -0400

    -_-

--- app/main.js
+++ app/main.js
@@ -4,7 +4,6 @@ var SearchBox = require('./components/SearchBox');
 var request = require('request');
 var $ = require('jQuery');
 
-
 $.get('/reddit/',
     function (data) {
         if(data == "" || data == null)
@@ -16,18 +15,3 @@ $.get('/reddit/',
             <SearchBox data={data}/>, document.getElementById('search')
         );
     });
-
-//request('http://reddit-search.herokuapp.com/reddit/', function (error, response, data) {
-//    if(data != "" && data != null)
-//        data = JSON.parse(data);
-//    else
-//        data = [];
-//    React.render(
-//        <SearchBox data={data}/>, document.getElementById('search')
-//    );
-//});
-
-
-
-
-
