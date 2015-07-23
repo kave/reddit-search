14ea4f315a664d6eb92dc550595e58bd27427367
etapau <etapau@gmail.com>
2015-07-22 23:50:54 -0400

    bug fix

--- app/main.js
+++ app/main.js
@@ -5,7 +5,10 @@ var request = require('request');
 
 request('http://localhost:4444/reddit/', function (error, response, data) {
     //console.log(data);
-    data = JSON.parse(data);
+    if(data != "" || data != null)
+        data = JSON.parse(data);
+    else
+        data = [];
     React.render(
         <SearchBox data={data}/>, document.getElementById('search')
     );
